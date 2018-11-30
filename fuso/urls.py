74cbe129386f01d01ea447968e22202228a6cfae
eli <s1400204@sendai-nct.jp>
2018-11-30 13:16:03 +0900

    Added views to create/edit fuso post inside the site.

--- fuso/urls.py
+++ fuso/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='yamashiro'),
     path('post/<int:pk>/', views.post_detail, name='michishio'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='asashio'),
 ]
